b28df5ff944c2374ed312d65e1f049d12d35dfcf
Reese <reese.sexton01@gmail.com>
2018-08-29 01:10:10 -0700

    Using template

--- 2_Templates/app/routes.py
+++ 2_Templates/app/routes.py
@@ -1,15 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : 'Reese'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Micrblog</title>
-        </head>
-        <body>
-            <h1>Hello, {0}!</h1>
-        </body>
-    </html>'''.format(user['username'])
+    return render_template('index.html', title='Home', user=user)
