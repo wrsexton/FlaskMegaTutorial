dce2b64c4babf8b963226140fd56353150f64d6f
William Reese Sexton <reese.sexton01@gmail.com>
2018-09-03 11:39:00 -0400

    Added routing for register

--- 5_user_logins/app/routes.py
+++ 5_user_logins/app/routes.py
@@ -43,3 +43,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
